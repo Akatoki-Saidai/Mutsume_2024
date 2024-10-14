9c7e9f7da4eba8ff36fa444cd3446a0b5601f1aa
n17y17 <N17Y17@outlook.com>
2024-10-14 19:45:00 +0900

    地図を外部から読み込むように変更

--- gui/start_gui.py
+++ gui/start_gui.py
@@ -4,22 +4,22 @@ import socket
 import ipaddress
 
 # 任意のサーバーにアクセスしIPを確認
-s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-s.connect(('8.8.8.8', 80))
-local_ip = ipaddress.ip_address(s.getsockname()[0])
+try:
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    s.connect(('1.1.1.1', 80))
+    local_ip = ipaddress.ip_address(s.getsockname()[0])
+except Exception as e:
+    local_ip = socket.gethostbyname_ex(socket.gethostname())[2][0]
 
 HOST, PORT = '', 8000
 
-try:
-    # 受信用のJSONファイルを作成
-    with open("./data_from_browser", "x") as f:
-        f.write('{"motor_l": 0, "motor_r": 0, "light": false, "buzzer": false }')
+# 受信用のJSONファイルを作成
+with open("./data_from_browser", "w") as f:
+    f.write('{"motor_l": 0, "motor_r": 0, "light": false, "buzzer": false }')
 
-    # 送信用のJSONファイルを作成
-    with open("./data_to_browser", "x") as f:
-        f.write('{\n    "motor_l": 0,\n    "motor_r": 0,\n    "lat": null,\n    "lon": null,\n    "grav": [null, null, null],\n    "mag": [null, null, null],\n    "map_coordinates_upper_left": [null, null],\n    "map_coordinates_lower_right": [null, null],\n    "local_ip": "' + str(local_ip) + ':' + str(PORT) + '"\n}')
-except:
-    pass
+# 送信用のJSONファイルを作成
+with open("./data_to_browser", "w") as f:
+    f.write('{\n    "motor_l": 0,\n    "motor_r": 0,\n    "light": false,\n    "buzzer": false,\n    "lat": null,\n    "lon": null,\n    "grav": [null, null, null],\n    "mag": [null, null, null],\n    "local_ip": "' + str(local_ip) + ':' + str(PORT) + '"\n}')
 
 class Handler(http.server.SimpleHTTPRequestHandler):
     def do_POST(self):        
@@ -32,7 +32,7 @@ class Handler(http.server.SimpleHTTPRequestHandler):
             self.wfile.write(f.read().encode('utf-8'))
 
 with socketserver.TCPServer((HOST, PORT), Handler) as httpd:
-    print("サーバーが稼働しました\n同じネットワーク内のブラウザで http://" + str(local_ip) + ":" + str(PORT) + "にアクセスしてください！")
+    print("サーバーが稼働しました！\n同じネットワーク内のブラウザで http://" + str(local_ip) + ":" + str(PORT) + " にアクセスしてください")
     httpd.serve_forever()
 
 ## 参考にしたサイト
